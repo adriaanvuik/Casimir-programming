eed7b3b43a26643093e304fa2a973a7deddc84a2
adriaanvuik <adriaanvuik@gmail.com>
2016-10-26 09:18:27 +0000

    use test module in script

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
+import sys
+sys.path.append('/home/joyvan/work/day3/Casimir-programming/')
+import test.py as tst
+
 print('whatever you wanna hear')
+
+r = 10
+circum = tst.circumference(r)
+print('circumference for r = ', r, 'is ', circum)
